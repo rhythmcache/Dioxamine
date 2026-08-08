428684ffddd793ed6e7d7bab0940cf85fa832cf6
rhythmcache <153998419+rhythmcache@users.noreply.github.com>
2026-08-08 16:03:51 +0530

    Added compile time stub

---
+++ src_ext/stub/android/app/ActivityThread.java
@@ -0,0 +1,13 @@
+package android.app;
+
+import android.content.Context;
+
+public class ActivityThread {
+    public static ActivityThread systemMain() {
+        return null;
+    }
+
+    public Context getSystemContext() {
+        return null;
+    }
+}
